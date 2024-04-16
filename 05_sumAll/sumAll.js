8c6cce6936facb2b28e704bb043cfa9212d5566f
Anuvind <119839716+anuvind10@users.noreply.github.com>
2024-04-16 12:24:25 +0530

    Exercise 05 solution

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,6 +1,27 @@
-const sumAll = function() {
+const sumAll = function(firstNumber, secondNumber) {
+    let sum = 0;
+    let startSum;
+    let endSum;
 
-};
+    //Only allow positive numbers
+    if (typeof(firstNumber) != "number" || typeof(secondNumber) != "number" || firstNumber < 0 || secondNumber < 0)  {
+        return "ERROR";
+    }
+    if (firstNumber < secondNumber) {
+        startSum = firstNumber;
+        endSum = secondNumber;
+    }
+    else{
+        startSum = secondNumber;
+        endSum = firstNumber;
+    }
+
+    for (let i = startSum; i <= endSum; i++) {
+        sum += i;
+    }
+
+    return sum;
+}
 
 // Do not edit below this line
 module.exports = sumAll;
