1a08481c51e4f05391644809644428e6d3892689
Anuvind <119839716+anuvind10@users.noreply.github.com>
2024-04-23 12:40:46 +0530

    Exercise 09 solution

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,22 @@
-const palindromes = function () {
+const palindromes = function (sentence) {
+    let reversedWord = '';
+    let actualWord = '';
 
+    //Split the sentence
+    let splitSentence = sentence.split(/(\W+)/);
+
+    //Extract just the words
+    let words = splitSentence.filter(sentence => /[a-zA-Z]/.test(sentence));
+
+    //Combine each words to form the new string
+    for (let i = 0; i < words.length; i++) {
+        actualWord += words[i];
+    }
+
+    //Create a new reversed string for comparison
+    reversedWord = actualWord.split('').reverse().join('');
+
+    return actualWord.toLowerCase() == reversedWord.toLowerCase() ? true : false;
 };
 
 // Do not edit below this line
