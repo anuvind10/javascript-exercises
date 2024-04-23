b161f6013dca7991f89be1984f4c05ac0374d5af
Anuvind <119839716+anuvind10@users.noreply.github.com>
2024-04-23 13:16:45 +0530

    Exercise 10 solution

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,6 +1,20 @@
-const fibonacci = function() {
+const fibonacci = function(number) {
+    let parsedNumber = parseInt(number);
 
+    if (parsedNumber == 0) return 0;
+    
+    if (parsedNumber < 0) return "OOPS"
+    
+    //Create the fibonacci sequence
+    let fibSequence = [1,1];
+    
+    for (let i = 2; i < parsedNumber; i++) {
+        fibSequence[i] = fibSequence[i-1] + fibSequence[i-2];
+    }
+
+    return fibSequence[fibSequence.length-1];
 };
 
 // Do not edit below this line
 module.exports = fibonacci;
+// fibonacci(5);
