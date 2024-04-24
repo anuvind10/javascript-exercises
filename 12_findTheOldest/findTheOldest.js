51c54406920d27f10772c23ddb3b0ab882551f87
Anuvind <119839716+anuvind10@users.noreply.github.com>
2024-04-24 16:00:22 +0530

    Exercise 11 and 12 solution

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,4 +1,27 @@
-const findTheOldest = function() {
+const findTheOldest = function(people) {
+    let age = [];
+    let oldest = 0;
+    let index;
+    let currentYear = new Date().getFullYear();
+
+    for (let i = 0; i < people.length; i++) {
+
+        //If year of death is undefined for a person, get age at current year
+        if (people[i].yearOfDeath == undefined) {
+          age[i] = currentYear - people[i].yearOfBirth;
+        }
+        else {
+          age[i] = people[i].yearOfDeath - people[i].yearOfBirth;
+        }
+
+        //Find the oldest age
+        if (age[i] > oldest) {
+          oldest = age[i];
+          index = i;
+        }
+    }
+
+    return people[index];
 
 };
 
